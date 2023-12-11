6385c5688d0fb9343ab549cdae5cb19780ec56ab
erasedvisuals <alessandro.nista.21@gmail.com>
2023-12-11 17:19:21 +0100

    v.0.9-need to add highscore carry

--- myScript.js
+++ myScript.js
@@ -19,7 +19,7 @@ function wrong(i) {
     document.getElementById("sep").classList.add('wrong')
     number.classList.add('wrong')
     tip[i].classList.add('wrong-text')
-    number.innerHTML = 'X'
+    number.innerHTML = 'x'
     // rimuove le classi ad animazione terminata, così da poterle riaggiungere
     setTimeout(() => {
         document.getElementById("sep").classList.remove('wrong')
@@ -29,8 +29,20 @@ function wrong(i) {
     }, 520)
 
 }
+// correct animation
+function correct(i) {
+    document.getElementById("sep").classList.add('correct')
+    number.classList.add('correct')
+    // tip text changes
+    setTimeout(() => {
+        tip[i].innerHTML = "Correct, want to play again?"
+        tip[i].classList.replace("text-white", "text-neon-g")
+        tip[i].classList.replace("text-neon-r", "text-neon-g")
+        number.innerHTML = guess
+    }, 750)
+}
 // disable all inputs exept for restart
-function disable(i){
+function disable(i) {
     input[i].disabled = true
     input[i].classList.remove("border-neon-cyan")
     check[i].disabled = true
@@ -38,14 +50,14 @@ function disable(i){
 
 // for con 0 e 1 dove 0 sta per comportamento con schermo xs e 1 lg
 for (let i = 0; i <= 1; i++) {
-    if (score[i].innerHTML <= 0){
+    if (score[i].innerHTML <= 0) {
         disable(i)
     }
     check[i].addEventListener('click', run)
     function run() {
         let value = input[i].value
         if (value < 1 || value > 20) {
-            wrong(i)       
+            wrong(i)
             tip[i].innerHTML = "Insert a valid number"
             tip[i].classList.replace("text-white", "text-neon-r")
             //sep wrong   
@@ -61,25 +73,18 @@ for (let i = 0; i <= 1; i++) {
             score[i].innerHTML = score[i].innerHTML - 1
             tip[i].classList.replace("text-white", "text-neon-r")
         } else {
-            document.getElementById("sep").classList.add('correct')
-            number.classList.add('correct')
-            // tip text changes
-            tip[i].innerHTML = "Correct, want to play again?"
-            tip[i].classList.replace("text-white", "text-neon-g")
-            tip[i].classList.replace("text-neon-r", "text-neon-g")
-            //mostra il numero
-            number.innerHTML = guess
+            correct(i)
             //aggiorna score
             highscore[i].innerHTML = score[i].innerHTML
             score[i].innerHTML = 20
             //disabilita tutto in modo da forzare il play again
             disable(i)
         }
-        if (score[i].innerHTML <= 0){
+        if (score[i].innerHTML <= 0) {
             tip[i].innerHTML = "You lost, try again"
             disable(i)
         }
     }
-    document.getElementsByClassName('play-again')[i].addEventListener('click', ()=>{window.location.reload()})
+    document.getElementsByClassName('play-again')[i].addEventListener('click', () => { window.location.reload() })
 }
 
